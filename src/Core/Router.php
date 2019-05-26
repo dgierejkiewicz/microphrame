c725c571d7c0afdd48f929ab54d84c4b530704e2
Dariusz <dariusz.gierejkiewicz@gmail.com>
2019-05-26 11:39:49 +0200

    Created Router class

---
+++ src/Core/Router.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Core;
+
+/**
+ * Class Router
+ * @package Core
+ */
+class Router
+{
+    /** @var array $routes */
+    protected $routes = [];
+
+    /** @var array $params */
+    protected $params = [];
+
+    /**
+     * Add route to the routing table
+     *
+     * @param $route
+     * @param $params
+     */
+    public function add(string $route, $params = [])
+    {
+        // Convert route to regular expression
+        $route = preg_replace('/\//','\\/', $route);
+
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/','(?P<\1>[a-z-]+)', $route);
+//        print_r($route);exit;
+
+        // Add start and end delimiters, and case sensitive flag
+        $route = '/^' . $route . '$/i';
+
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoutes() : array
+    {
+        return $this->routes;
+    }
+
+    /**
+     * @param $url
+     * @return bool
+     */
+    public function match($url)
+    {
+//        $regExp = '/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/';
+
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+                /** @var array $params */
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,29 @@
 <?php
-echo "Hello World from Microphrame v. 0.0.1";
-echo "<br \>";
-echo "<pre>";
-echo var_export($_SERVER);
+
+require_once '../Core/Router.php';
+echo 'hello from microphrame v. 0.0.1';
+echo PHP_EOL;
+
+$router = new \Core\Router();
+
+//echo get_class($router);
+
+// Add routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('admin/{controller}/{action}');
+
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
